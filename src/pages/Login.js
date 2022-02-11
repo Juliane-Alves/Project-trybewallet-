163eb132fd53f8f946dfe89c72b3491598228b58
Juliane Alves <julianeju3654@gmail.com>
2022-02-11 11:30:07 -0300

    Tentativa de test requisito 1, localmente npm test com problemas

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getLogin } from '../actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      password: '',
+      buttonIsDisabled: true,
+    };
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.btnValidate);
+  }
+
+  btnValidate = () => {
+    const MIN_CHARACTERS = 6;
+    const { email, password } = this.state;
+    // uso do toLowerCase https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
+    // uso do match https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/match
+    const validateEmail = email.includes('@' && '.com');
+    const passwordCheck = password.length >= MIN_CHARACTERS;
+    this.setState({ buttonIsDisabled: !(validateEmail && passwordCheck) });
+  }
+  // Habitlitação do botão de acordo com email valido e quantidade de caracters minimos;
+
   render() {
-    return <div>Login</div>;
+    const { email, password, buttonIsDisabled } = this.state;
+    const { getLoginSave, history } = this.props;
+    return (
+      <div>
+        <h1>Trybe</h1>
+        <section>
+          <label htmlFor="email">
+            Email
+            <input
+              name="email"
+              type="text"
+              id="email"
+              data-testid="email-input"
+              value={ email }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <label htmlFor="password">
+            Password
+            <input
+              name="password"
+              type="password"
+              id="password"
+              data-testid="password-input"
+              value={ password }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            disabled={ buttonIsDisabled }
+            onClick={ () => {
+              getLoginSave(email); // botão habilitado para o clique, email valido enviado salvo no state
+              history.push('/carteira'); // no clique do botão a rota é redirecionada para carteira;
+            } }
+          >
+            Entrar
+          </button>
+        </section>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  getLoginSave: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  getLoginSave: (email) => dispatch(getLogin(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
